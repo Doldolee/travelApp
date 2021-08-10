06c94dd5e7557d776f691900e76421383056f5b9
임유석 <seook6853@naver.com>
2021-08-10 22:17:21 +0900

    MainSearch

--- app/src/main/java/com/seook/travelapp_hanium/main_search/MainSearchActivity.kt
+++ app/src/main/java/com/seook/travelapp_hanium/main_search/MainSearchActivity.kt
@@ -1,14 +1,18 @@
 package com.seook.travelapp_hanium.main_search
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.seook.travelapp_hanium.MainActivity
 import com.seook.travelapp_hanium.R
 import com.seook.travelapp_hanium.api.ApiData
 import com.seook.travelapp_hanium.api.ApiRequest
 import com.seook.travelapp_hanium.api.BASE_URL
+import com.seook.travelapp_hanium.databinding.ActivityMainSearchBinding
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -18,8 +22,11 @@ import java.lang.Exception
 
 class MainSearchActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainSearchBinding
+
     private lateinit var rvAdapter: SearchRVAdapter
     private var items = ArrayList<SearchModel>()
+//    private var itemsx = mutableListOf<String>()
     private val TAG = MainSearchActivity::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,13 +34,17 @@ class MainSearchActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_search)
 
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main_search)
 
+        binding.backBtn.setOnClickListener {
+            finish()
+        }
 
         makeApiRequest()
 
-        rvAdapter = SearchRVAdapter(items)
+        rvAdapter = SearchRVAdapter(baseContext, items)
 
-        val rv :RecyclerView = findViewById(R.id.rv)
+        val rv :RecyclerView = binding.rv
         rv.adapter = rvAdapter
         rv.layoutManager = LinearLayoutManager(this)
 
@@ -41,6 +52,7 @@ class MainSearchActivity : AppCompatActivity() {
     }
 
     private fun makeApiRequest(){
+        Log.d(TAG, "startAPI")
         val api = Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
@@ -49,12 +61,14 @@ class MainSearchActivity : AppCompatActivity() {
 
         GlobalScope.launch(Dispatchers.IO) {
             try {
+                Log.d(TAG, "tryAPI")
                 var response = api.getNationList()
 //                Log.d("Main", "Size: ${response.name}")
 //                items.add(SearchModel("a",response.name))
                 for (data in response){
 //                    Log.d("Main", "Size: ${data.name}")
                     items.add(SearchModel("url", data.name))
+                    Log.d(TAG, "addApi")
                 }
                 rvAdapter.notifyDataSetChanged()
 
--- app/src/main/java/com/seook/travelapp_hanium/main_search/SearchRVAdapter.kt
+++ app/src/main/java/com/seook/travelapp_hanium/main_search/SearchRVAdapter.kt
@@ -1,13 +1,21 @@
 package com.seook.travelapp_hanium.main_search
 
+import android.content.Context
+import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
+import com.seook.travelapp_hanium.MainActivity
 import com.seook.travelapp_hanium.R
+import com.seook.travelapp_hanium.fragments.SearchFragment
 
-class SearchRVAdapter(val items: ArrayList<SearchModel>):RecyclerView.Adapter<SearchRVAdapter.Viewholder>() {
+class SearchRVAdapter( val context: Context, val items: ArrayList<SearchModel>):RecyclerView.Adapter<SearchRVAdapter.Viewholder>() {
+
+    private val TAG = SearchRVAdapter::class.java.simpleName
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchRVAdapter.Viewholder {
         val v = LayoutInflater.from(parent.context).inflate(R.layout.main_rv_item, parent, false)
         return Viewholder(v)
@@ -23,14 +31,18 @@ class SearchRVAdapter(val items: ArrayList<SearchModel>):RecyclerView.Adapter<Se
 
     inner class Viewholder(itemView: View):RecyclerView.ViewHolder(itemView){
         fun bindItems(item:SearchModel){
+            Log.d(TAG, "bind함수 실행")
             val cityTitle = itemView.findViewById<TextView>(R.id.cityArea)
 
             cityTitle.text = item.cityTitle
 
-
-
-
+            itemView.setOnClickListener {
+                //이 부분 SearchFragment로 보내고 싶은데 모르겠습니다.
+                val intent  = Intent(context, MainActivity::class.java)
+                intent.putExtra("city", item.cityTitle)
+                itemView.context.startActivity(intent)
+//            }
 
         }
     }
-}
+}}
